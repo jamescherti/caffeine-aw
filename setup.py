8a886f47d5a4748c2b573534cfcfc3d8c57783da
Reuben Thomas <rrt@sc3d.org>
2020-08-31 17:47:21 +0100

    Fix "distutils bdist" (LP#1784418)

--- setup.py
+++ setup.py
@@ -7,11 +7,12 @@ import shutil
 import subprocess
 
 ROOT_DIR = dirname(abspath(__file__))
-SHARE_PATH = join(ROOT_DIR, "share")
 PO_DIR = 'translations'
 VERSION = open(join(ROOT_DIR, "VERSION")).read().strip()
 
 # Update the translations
+if not exists(PO_DIR):
+    os.makedirs(PO_DIR)
 subprocess.check_call(["xgettext", "-o", join(PO_DIR, "caffeine-indicator.pot"), "--language=python", "--from-code=UTF-8", "caffeine-indicator"])
 subprocess.check_call(["./compile_translations.py", "caffeine-indicator", PO_DIR])
 
@@ -19,9 +20,10 @@ subprocess.check_call(["./compile_translations.py", "caffeine-indicator", PO_DIR
 blacklist = ['index.theme']
 
 data_files = []
-for path, dirs, files in os.walk(SHARE_PATH):
-    data_files.append(tuple((path.replace(SHARE_PATH,"share", 1),
-        [join(path, file) for file in files if file not in blacklist])))
+for path, _, files in os.walk("share"):
+    if len(files) > 0:
+        data_files.append(tuple((path,
+            [join(path, file) for file in files if file not in blacklist])))
 
 desktop_name = "caffeine.desktop"
 desktop_file = join("share", "applications", desktop_name)
@@ -30,6 +32,7 @@ if not exists(autostart_dir):
     os.makedirs(autostart_dir)
 shutil.copy(desktop_file, autostart_dir)
 data_files.append(tuple(("/" + autostart_dir, [join(autostart_dir, desktop_name)])))
+data_files.append(tuple(("", ["VERSION", "compile_translations.py"])))
 
 setup(name="caffeine",
     version=VERSION,
