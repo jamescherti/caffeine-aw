0fc3ca4f5e5b3575cf03081441fe5995420ecc88
rrt@sc3d.org <>
2022-03-28 17:15:48 +0100

    Simplify packaging (fix LP#1784418)
    The problem was caused by two files: VERSION and compile_translations.py.
    Neither should be in a bdist, but both are required in the sdist.
    
    It’s tricky to fix this bug directly, so instead remove the need for these
    files: fold them both into setup.py.
    
    Also, remove share/icons/hicolor/index.theme, which served no purpose, and
    as a result replace some code in setup.py with a single-line MANIFEST.in.

--- setup.py
+++ setup.py
@@ -3,28 +3,42 @@
 from distutils.core import setup
 import os
 from os.path import join, abspath, dirname, exists
+import sys
 import shutil
 import subprocess
 
-ROOT_DIR = dirname(abspath(__file__))
-PO_DIR = 'translations'
-VERSION = open(join(ROOT_DIR, "VERSION")).read().strip()
-
 # Update the translations
+PO_DIR = 'translations'
 if not exists(PO_DIR):
     os.makedirs(PO_DIR)
 subprocess.check_call(["xgettext", "-o", join(PO_DIR, "caffeine-indicator.pot"), "--language=python", "--from-code=UTF-8", "caffeine-indicator"])
-subprocess.check_call(["./compile_translations.py", "caffeine-indicator", PO_DIR])
 
-# don't trash the system icons!
-blacklist = ['index.theme']
+def compile_catalog(po_dir, prg_name):
+    po_files = []
+    for dirpath, dirnames, filenames in os.walk(po_dir):
+        for file in filenames:
+            if file.split('.')[-1] == "po":
+                po_files.append(os.path.join(dirpath, file))
 
+    for po in po_files:
+        lang = po.split('/')[-1]
+        print("Compiling for Locale: "+"".join(lang.split(".")[:-1]))
+        lang = lang.split('-')[-1]
+        lang = lang.split('.')[0]
+        lang = lang.strip()
+        if not lang:
+            continue
+
+        lang_lc_dir = os.path.join('share', 'locale', lang, 'LC_MESSAGES')
+        if not os.path.isdir(lang_lc_dir):
+            os.makedirs(lang_lc_dir)
+
+        subprocess.check_call(["msgfmt", po, "-o", os.path.join(lang_lc_dir,prg_name+".mo")])
+
+compile_catalog(PO_DIR, "caffeine-indicator")
+
+# Add extra data files
 data_files = []
-for path, _, files in os.walk("share"):
-    if len(files) > 0:
-        data_files.append(tuple((path,
-            [join(path, file) for file in files if file not in blacklist])))
-
 desktop_name = "caffeine.desktop"
 desktop_file = join("share", "applications", desktop_name)
 autostart_dir = join("etc", "xdg", "autostart")
@@ -32,15 +46,14 @@ if not exists(autostart_dir):
     os.makedirs(autostart_dir)
 shutil.copy(desktop_file, autostart_dir)
 data_files.append(tuple(("/" + autostart_dir, [join(autostart_dir, desktop_name)])))
-data_files.append(tuple(("", ["VERSION", "compile_translations.py"])))
 
 setup(name="caffeine",
-    version=VERSION,
+    version="2.9.8",
     description="Stop the desktop from becoming idle in full-screen mode.",
     license="GPLv3",
     author="The Caffeine Developers",
     author_email="rrt@sc3d.org",
     url="https://launchpad.net/caffeine",
     data_files=data_files,
-    scripts=["caffeine", "caffeinate", "caffeine-indicator"]
+    scripts=["caffeine", "caffeinate", "caffeine-indicator"],
     )
