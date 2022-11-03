4d333f5579a3b2491deec620df98c7e83773db48
James Cherti <60946298+jamescherti@users.noreply.github.com>
2022-11-03 14:48:32 -0400

    Add requirements and fix '.gitignore'.

--- setup.py
+++ setup.py
@@ -69,4 +69,5 @@ setup(name="cups-of-caffeine",
 
     scripts=["caffeine", "caffeinate", "caffeine-indicator"],
     py_modules=[], # Workaround for setuptools >= 61.0; see https://bugs.launchpad.net/caffeine/+bug/1981419
+    install_requires=["pygobject>=3.1.1,<4.0", "ewmh>=0.1.4"],
     )
